5c14c632e08f2e2729d4d6680112a66183093e4e
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2023-09-15 16:31:42 +0530

    Add files via upload

---
+++ assi6_4.1.py
@@ -0,0 +1,8 @@
+#Accept user's name and birth year. Print user's name and age.
+#Hint - age = 2021 - birth year
+username = input("enter username:")
+birthYear = int(input("enter your date of birth:"))
+age = 2022 - birthYear
+print("username is", username)
+print("age is", age)
+
