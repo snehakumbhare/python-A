0f50329af66a17c0e4f12f159a4093aa2020199b
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2023-09-20 16:39:45 +0530

    Add files via upload

---
+++ assi6_3.py
@@ -0,0 +1,7 @@
+# Declare the value as x=True and y=false and test the condition for and, or & not and produce the result?
+#x=True 
+#y="false
+
+print((75>45) and (18>56))
+print((75>45) or (18>56))
+print(not(75>45))
