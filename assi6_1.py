7c4b32d5b686c2c1e2c7823d327d73c1382f5c67
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2023-09-21 16:48:04 +0530

    Add files via upload

---
+++ assi6_1.py
@@ -0,0 +1,19 @@
+#Declare a=60 ,b=13 and c= 0 and perform all the bitwise operation where c will be storing 
+# the result on the interpreter showing the output.
+
+a=60
+b=13
+c=a&b
+print(c)
+c=a|b
+print(c)
+c=(a^b)
+print(c)
+c=(~a)
+print(c)
+c=(~b)
+print(c)
+c=(a<<b)
+print(c)
+c=(a>>b)
+print(c)
