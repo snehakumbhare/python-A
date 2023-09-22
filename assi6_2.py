1022152b5dd3c9857df46a8f007ac6c991586d51
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2023-09-22 15:09:35 +0530

    Add files via upload

---
+++ assi6_2.py
@@ -0,0 +1,9 @@
+# Take the 2 input from the user assign the value and perform the additon,
+#  and then add and assign to the result and lastly multiply and assign to the same value.
+
+a=5
+a += 3
+print(a)
+
+a *= 3
+print(a)
