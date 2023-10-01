55380bc39f57360dbb97df4933a2021b7e70bb33
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2023-10-01 13:58:53 +0530

    Add files via upload

---
+++ pallindrome.py
@@ -0,0 +1,15 @@
+#wap to create a function to find palindrome number:
+n=int(input("Enter a number:"))
+n1=n
+rev=0
+
+while(n!=0):
+    rem=n%10
+    rev=(rev*10)+rem
+    n=n//10
+    
+if(n1==rev):
+    print(f"{n1} is a palindromic number")
+    
+else:
+    print(f"{n1} is not a palindromic number")
