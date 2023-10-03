492077026b16b00a49acd935bb1db64b854c9791
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2023-10-03 13:47:10 +0530

    Update Anagrams.py

--- Anagrams.py
+++ Anagrams.py
@@ -9,8 +9,8 @@ def check(s1, s2):
         print("The strings aren't anagrams.")        
         
 s1 = input("Enter string1: ")
-# input1: "listen"
+
 s2 = input("Enter string2: ")
-# input2: "silent"
+
 check(s1, s2)
-# Output: the strings are anagrams.
+
