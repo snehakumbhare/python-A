b938ea3a8c9aa3b311f5e0d78e3f86a6399a5d69
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2023-10-03 13:52:00 +0530

    Add files via upload

---
+++ pg.py
@@ -0,0 +1,15 @@
+#Write a program to print the following pattern.
+#*
+#* *
+#* * *
+#* * * *
+#* * * * *
+
+
+def myfunc(n):
+    for i in range(0, n):
+        for j in range(0, i+1):
+            print("* ",end="")
+        print("\r")
+n = 5
+myfunc(n)
