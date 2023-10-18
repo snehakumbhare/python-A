06dd19dbe7258e353e18b49cbd2c348a50fc555c
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2023-10-18 13:17:42 +0530

    Add files via upload

---
+++ emogi.py
@@ -0,0 +1,11 @@
+#Print Emojis using Python
+#The emoji.emojize method helps you write the description of any emoji inside “::”
+#popular emojis:
+#:thumbs_up:
+#:red_heart:
+#:smiling_face:
+import emoji
+
+print(emoji.emojize("I love reading books:books:"))
+print(emoji.emojize("Some people have a very sensitive heart:red_heart:, please be kind with them.:hibiscus:"))
+print(emoji.emojize("I love chirstmas:Christmas_tree:"))
