4375b2f0ffb6e554e37c8d790aeb5e7b8dda2b4b
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2023-10-25 12:36:13 +0530

    Add files via upload

---
+++ cuss1.py
@@ -0,0 +1,9 @@
+#Remove Cuss Words using Python
+#pip install better_profanity
+from better_profanity import profanity
+text = "Please leave me alone and just piss off"
+text = "Why am I doing all the fucking work?"
+text = "He’s so fucking stupid!"
+
+censored = profanity.censor(text)
+print(censored)
