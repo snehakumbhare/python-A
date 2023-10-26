a23c629fc9bb316cee2ae421ea95e02eff5e76a5
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2023-10-26 14:59:37 +0530

    Add files via upload

---
+++ correctspelling1.py
@@ -0,0 +1,11 @@
+#Correct Spellings using Python
+#pip install pyspellchecker in your command prompt or terminal
+from spellchecker import SpellChecker
+corrector = SpellChecker()
+
+word = input("Enter a Word : ")
+if word in corrector:
+    print("Correct")
+else:
+    correct_word = corrector.correction(word)
+    print("Correct Spelling is ", correct_word)
