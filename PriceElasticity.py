3b40d7e9b3368f7e7c7080a33cd1b53f77095fb5
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2023-10-30 09:53:03 +0530

    Update PriceElasticity.py

--- PriceElasticity.py
+++ PriceElasticity.py
@@ -1,5 +1,6 @@
 #Price Elasticity of Demand using Python
 # pip inatall pandas
+
 import pandas as pd
 data = pd.DataFrame({"Demand": [20, 30, 31, 33, 30, 33, 35], 
                      "Price": [2000, 1800, 1850, 1700, 1800, 1700, 1600]})
