533030e41ee70b4ee7e783de2a174837b7ca30a2
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2023-11-05 21:10:38 +0530

    Add files via upload

---
+++ voice1.py
@@ -0,0 +1,14 @@
+#Voice Recorder using Python
+#pip install sounddevice
+#pip install SciPy
+import sounddevice
+from scipy.io.wavfile import write
+
+def voice_recorder(seconds, file):
+    print("Recording Started…")
+    recording = sounddevice.rec((seconds * 44100), samplerate= 44100, channels=2)
+    sounddevice.wait()
+    write(file, 44100, recording)
+    print("Recording Finished")
+
+voice_recorder(10, "record.wav")
