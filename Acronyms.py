04df10b30754bf49534498107df66f2e50057bb3
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2023-11-07 16:24:23 +0530

    Add files via upload

---
+++ Acronyms.py
@@ -0,0 +1,8 @@
+#Create Acronyms using Python
+# ‘a’ to store the acronym of a phrase.
+user_input = str(input("Enter a Phrase: "))
+text = user_input.split()
+a = " "
+for i in text:
+    a = a+str(i[0]).upper()
+print(a)
