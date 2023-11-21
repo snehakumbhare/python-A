2e4551e955e905518aa9d3e47876536e5940511a
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2023-11-21 15:54:26 +0530

    Add files via upload

---
+++ average1.py
@@ -0,0 +1,17 @@
+#Python program to find the average of numbers with explanations16
+size=int(input("Enter the number of elements you want in array: "))
+arr=[]
+#taking input of the list
+for i in range(0,size):
+    elem=int(input("Please give value for index "+str(i)+": "))
+    arr.append(elem)
+#taking average of the elements of the list
+avg=sum(arr)/size
+print("Average of the array elements is ",avg)
+#Enter the number of elements you want in array: 5
+#Please give value for index 0: 9
+#Please give value for index 1: 5
+#Please give value for index 2: 7
+#Please give value for index 3: 88
+#Please give value for index 4: 21
+#Average of the array elements is  26.0
