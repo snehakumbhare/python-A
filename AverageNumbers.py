2cdf685f24054d3fd6bf1ccb95233b335d2526fd
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2023-11-21 15:56:16 +0530

    Update AverageNumbers.py

--- AverageNumbers.py
+++ AverageNumbers.py
@@ -6,6 +6,7 @@ arr=[]
 for i in range(0,size):
     elem=int(input("Please give value for index "+str(i)+": "))
     arr.append(elem)
+    
 #taking average of the elements of the list
 avg=sum(arr)/size
 print("Average of the array elements is ",avg)
