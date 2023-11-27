081197c95936ff6a462ad284e9a3596cc0c792f0
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2023-11-27 16:20:51 +0530

    Add files via upload

---
+++ octalnum1.py
@@ -0,0 +1,13 @@
+#Python Program to convert Decimal to Octal number33
+def decimal_to_octal(decimal):
+    if decimal == 0:
+        return ""
+    quotient = decimal // 8
+    remainder = decimal % 8
+    return decimal_to_octal(quotient) + str(remainder)
+    
+decimal = int(input("Please Enter a decimal number: "))
+octal = decimal_to_octal(decimal)
+print("Octal number is ", octal, " for ", decimal)
+#Please Enter a decimal number: 157
+#Octal number is  235  for  157
