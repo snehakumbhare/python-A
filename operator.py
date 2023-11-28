21c66253f890477c33f522aa698b4ffea1d03150
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2023-11-28 16:19:50 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,12 @@
+#Python Program to add two numbers without addition operator
+def add_numbers(x, y):
+    while y != 0:
+        temp = x & y
+        x = x ^ y
+        y = temp << 1
+    return x
+ 
+num1 = 10
+num2 = 15
+sum_result = add_numbers(num1, num2)
+print(f"Sum of {num1} and {num2} is: {sum_result}")
