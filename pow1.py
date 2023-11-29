33899bcf34a657769e0ff11877a9c7342180c931
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2023-11-29 17:37:27 +0530

    Add files via upload

---
+++ pow1.py
@@ -0,0 +1,9 @@
+#Python program to calculate the power using ‘pow()’ method23
+#taking 2 integer as input from the user
+base = int(input("Enter the value for base :"))
+exponent = int(input("Enter the value for exponent :"))
+print(base,"to power ",exponent,"=",end = ' ')
+print(pow(base,exponent))
+#Enter the value for base :5
+#Enter the value for exponent :4
+#5 to power  4 = 625
