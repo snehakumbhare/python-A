31d64512e8f74696171706082e847844de4d6676
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2023-12-02 16:12:13 +0530

    Update PowerWLoop.py

--- PowerWLoop.py
+++ PowerWLoop.py
@@ -4,6 +4,7 @@ base = int(input("Enter the value for base :"))
 exponent = int(input("Enter the value for exponent :"))
 result=1;
 print(base,"to power ",exponent,"=",end = ' ')
+
 #using while loop with a condition that come out of while loop if exponent is 0
 while exponent != 0:
     result = base * result
