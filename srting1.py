c510669467bbdef04c2e2a205ec4a6a29c347216
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2023-12-03 16:37:39 +0530

    Add files via upload

---
+++ srting1.py
@@ -0,0 +1,12 @@
+#Python Program to Replace First Vowel With ‘-‘ in String15
+def replace_vowel(string):
+    vowels = "AEIOUaeiou"
+    for i in range(len(string)):
+        if string[i] in vowels:
+            string = string[:i] + '-' + string[i+1:]
+            break
+    return string
+
+string = input("Please enter a string: ")
+print("Original String:", string)
+print("Modified String:", replace_vowel(string)) 
