9d792ad481e1ae33e454d1d13954a818f070f030
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2023-12-11 15:41:25 +0530

    Add files via upload

---
+++ matrix1.py
@@ -0,0 +1,36 @@
+#Python Program to Add Two Matrices
+# Program to add two matrices using nested loop
+ 
+X = [[1,2,3],
+    [4 ,5,6],
+    [7 ,8,9]]
+ 
+Y = [[9,8,7],
+    [6,5,4],
+    [3,2,1]]
+ 
+ 
+result = [[0,0,0],
+        [0,0,0],
+        [0,0,0]]
+ 
+# iterate through rows
+for i in range(len(X)):   
+# iterate through columns
+    for j in range(len(X[0])):
+        result[i][j] = X[i][j] + Y[i][j]
+ 
+for r in result:
+    print(r)
+
+# Program to add two matrices
+# using list comprehension
+#result = [[X[i][j] + Y[i][j]  for j in range
+#(len(X[0]))] for i in range(len(X))]
+  
+#for r in result:
+    #print(r)
+
+#Program to add two matrices
+# using zip()
+  
