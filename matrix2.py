bf8d676daec78723bc901ace96413fae159069d0
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2023-12-12 16:29:40 +0530

    Add files via upload

---
+++ matrix2.py
@@ -0,0 +1,33 @@
+# Program to multiply two matrices using list comprehension
+
+# take a 3x3 matrix
+A = [[12, 7, 3],
+    [4, 5, 6],
+    [7, 8, 9]]
+ 
+# take a 3x4 matrix
+B = [[5, 8, 1, 2],
+    [6, 7, 3, 0],
+    [4, 5, 9, 1]]
+ 
+# result will be 3x4
+#result = [[sum(a * b for a, b in zip(A_row, B_col)) 
+                        #for B_col in zip(*B)]
+                                #for A_row in A]
+ 
+#for r in result:
+  #  print(r)
+
+# Program to multiply two matrices (vectorized implementation)
+import numpy as np 
+# take a 3x3 matrix
+# result will be 3x4
+ 
+result= [[0,0,0,0],
+        [0,0,0,0],
+        [0,0,0,0]]
+ 
+result = np.dot(A,B)
+ 
+for r in result:
+    print(r)
