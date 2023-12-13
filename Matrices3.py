f328267864aaf921624f8c5bc5f27adf86e60f9f
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2023-12-13 16:14:01 +0530

    Update Matrices3.py

--- Matrices3.py
+++ Matrices3.py
@@ -22,6 +22,7 @@ test_list = [[1, 4, 5], [7, 3], [4], [46, 7, 3]]
 print("The original list : " + str(test_list))
  
 # using list comprehension + loop
+
 # Matrix Product
 res = prod([ele for sub in test_list for ele in sub])
 
