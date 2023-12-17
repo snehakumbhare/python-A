6f2b7a56820856f2b2e907d8f1fb04f76b5ef8b9
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2023-12-17 16:58:32 +0530

    Update Dictionary1.py

--- Dictionary1.py
+++ Dictionary1.py
@@ -15,6 +15,7 @@ print("The original dictionary is : " + str(test_dict))
 # Extract Unique values dictionary values
 # Using set comprehension + values() + sorted()
 res = list(sorted({ele for val in test_dict.values() for ele in val}))
+
 # Using chain() + sorted() + values()
 res = list(sorted(set(chain(*test_dict.values())))) 
 # printing result
