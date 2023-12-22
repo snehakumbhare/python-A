480b951e978512bf0116a7d844474cfbaf3ae8a7
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2023-12-22 16:53:42 +0530

    Update Tuple2.py

--- Tuple2.py
+++ Tuple2.py
@@ -9,7 +9,6 @@ list1 = [1, 2, 5, 6]
  
 # using list comprehension to iterate each
 # values in list and create a tuple as specified
-
 #res = [(val, pow(val, 3)) for val in list1]
 res = [(val, val**3) for val in list1]
 
