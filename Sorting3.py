07b5485a59f137f844f9aa4f45765d4a4bc7eeec
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2023-12-29 14:14:49 +0530

    Update Sorting3.py

--- Sorting3.py
+++ Sorting3.py
@@ -6,7 +6,6 @@
 # At the very end of partition() function, the pointer is swapped with the pivot
 # to come up with a "sorted" nums relative to the pivot
  
- 
 # Function to find the partition position
 def partition(array, low, high):
  
