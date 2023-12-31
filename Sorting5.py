46ac6d0d2e56f308ea1386ce4c718f717674c34f
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2023-12-31 20:01:31 +0530

    Update Sorting5.py

--- Sorting5.py
+++ Sorting5.py
@@ -12,6 +12,7 @@ def selectionSort(array, size):
             # select the minimum element in every iteration
             if array[j] < array[min_index]:
                 min_index = j
+                
          # swapping the elements to sort the array
         (array[ind], array[min_index]) = (array[min_index], array[ind])
  
