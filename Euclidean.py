47c075a97ecd2effaeb098730527a76f0d1e34c7
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-01-15 16:22:16 +0530

    Update Euclidean.py

--- Euclidean.py
+++ Euclidean.py
@@ -10,7 +10,7 @@ def gcdExtended(a, b):
         return b,0,1
              
     gcd,x1,y1 = gcdExtended(b%a, a) 
-     
+
     # Update x and y using results of recursive 
     # call 
     x = y1 - (b//a) * x1 
