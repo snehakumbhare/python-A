f9127b790f4be66685e31a28a8d13110f78b9fee
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-01-22 16:30:12 +0530

    Add files via upload

---
+++ string5.py
@@ -0,0 +1,20 @@
+#String slicing in Python to Rotate a String
+# Function to rotate string left and right by d length
+ 
+def rotate(input,d):
+ 
+    # slice string in two parts for left and right
+    Lfirst = input[0 : d]
+    Lsecond = input[d :]
+    Rfirst = input[0 : len(input)-d]
+    Rsecond = input[len(input)-d : ]
+ 
+    # now concatenate two parts together
+    print ("Left Rotation : ", (Lsecond + Lfirst) )
+    print ("Right Rotation : ", (Rsecond + Rfirst))
+ 
+# Driver program
+if __name__ == "__main__":
+    input = 'SNEHAKUMBHARE'
+    d=2
+    rotate(input,d)
