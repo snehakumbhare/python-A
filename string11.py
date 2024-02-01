279174dac3ea201b3a1e3ba15a522b38906ae9a3
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-02-01 15:55:33 +0530

    Add files via upload

---
+++ string11.py
@@ -0,0 +1,29 @@
+#Python program to find uncommon words from two Strings
+# Python3 program to find a list of uncommon words
+ 
+# Function to return all uncommon words
+ 
+ 
+def UncommonWords(A, B):
+ 
+    # count will contain all the word counts
+    count = {}
+ 
+    # insert words of string A to hash
+    for word in A.split():
+        count[word] = count.get(word, 0) + 1
+ 
+    # insert words of string B to hash
+    for word in B.split():
+        count[word] = count.get(word, 0) + 1
+ 
+    # return required list of words
+    return [word for word in count if count[word] == 1]
+ 
+ 
+# Driver Code
+A = "python coding"
+B = "Learning from python coding"
+ 
+# Print required answer
+print(UncommonWords(A, B))
