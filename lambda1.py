c8c4242393c14f2c630423891eb8d4921a6c779c
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-02-12 16:10:32 +0530

    Add files via upload

---
+++ lambda1.py
@@ -0,0 +1,21 @@
+#Lambda with if but without else in Python
+# Lambda function with if but without else.
+# square = lambda x : x*x if(x > 0) 
+ 
+#print(square(6))
+
+# Lambda function with if-else
+square = lambda x : x*x if(x > 0) else None
+ 
+print(square(4))
+
+# Example of lambda function using if-else
+mod = lambda x : x if(x >= 0) else -x
+#mod = lambda x : x if(x >= 0)
+print(mod(-1))
+
+# Example of lambda function using if-else
+max = lambda a, b : a if(a > b) else b
+#max = lambda a, b : x if(a > b)
+print(max(1, 2))
+print(max(10, 2))
