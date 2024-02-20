3cad4a115b7f3d8d4a8d683e167a1a2ae85e0895
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-02-20 16:28:28 +0530

    Update labellayout.py

--- labellayout.py
+++ labellayout.py
@@ -10,6 +10,7 @@ parent.title("Vertical Labels with Pack")
 label1 = tk.Label(parent, text="Python Exercises", padx=25, pady=10)
 label2 = tk.Label(parent, text="Java Exercises", padx=25, pady=10)
 label3 = tk.Label(parent, text="C++ Exercises", padx=25, pady=10)
+
 # Use the Pack geometry manager to arrange labels vertically
 label1.pack(side="top")
 label2.pack(side="top")
