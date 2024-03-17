dfea9a456ab04ce5bfe42143a1b6b4b95ea340a3
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-03-17 15:09:29 +0530

    Add files via upload

---
+++ tkwidgets.py
@@ -0,0 +1,15 @@
+#Python tkinter widgets Exercise: Create a Combobox with three options using tkinter module
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+my_str_var = tk.StringVar()
+
+my_combobox = ttk.Combobox(
+    root, textvariable = my_str_var,
+    values=["PHP", "Java", "Python" ,"SQL", "Django", "Angular10", "Web Designing",])
+
+my_combobox.pack()
+root.mainloop()
+
+
