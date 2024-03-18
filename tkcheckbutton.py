f940ab5ea435a8d077c98223046de19e3ff02a42
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-03-18 15:30:20 +0530

    Add files via upload

---
+++ tkcheckbutton.py
@@ -0,0 +1,13 @@
+#Python tkinter widgets Exercise: Create a Checkbutton widget using tkinter module
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+my_boolean_var = tk.BooleanVar()
+
+my_checkbutton = ttk.Checkbutton(
+    text="Check when the option True",
+    variable=my_boolean_var
+)
+my_checkbutton.pack()
+root.mainloop()
