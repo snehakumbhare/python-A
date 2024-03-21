4bc6a93c17234eca266aee0835f2fc2c13ef1c43
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-03-21 15:54:37 +0530

    Add files via upload

---
+++ tkcuinput.py
@@ -0,0 +1,19 @@
+#Python Tkinter: Creating a custom input dialog
+import tkinter as tk
+from tkinter import simpledialog
+
+def show_name_dialog():
+    name = simpledialog.askstring("Input", "Input your name:")
+    if name:
+        name_label.config(text=f"Hello, {name}!")
+
+parent = tk.Tk()
+parent.title("Input Dialog Example")
+
+get_name_button = tk.Button(parent, text="Get Name", command=show_name_dialog)
+get_name_button.pack(padx=20, pady=20)
+
+name_label = tk.Label(parent, text="", padx=20, pady=10)
+name_label.pack()
+
+parent.mainloop()
