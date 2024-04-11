02d47936e734e56e4a8f72f6404ec1a87e7b5e83
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-04-11 16:11:46 +0530

    Add files via upload

---
+++ colourpy.py
@@ -0,0 +1,6 @@
+#Python: Display the first and last colors from a given list
+# Create a list called 'color_list' containing color names
+color_list = ["Red", "Green", "White", "Black"]
+# Print the first and last elements of the 'color_list' using string formatting
+# The '%s' placeholders are filled with the values of 'color_list[0]' (Red) and 'color_list[-1]' (Black)
+print("%s %s" % (color_list[0], color_list[-1]))
