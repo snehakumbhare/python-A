0be483cfc7ab975ee682fa5b11baccd8f3d34b06
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-04-12 15:31:53 +0530

    Add files via upload

---
+++ examschedule.py
@@ -0,0 +1,7 @@
+#Python: Display a sample examination schedule
+# Define a tuple called 'exam_st_date' containing the exam start date in the format (day, month, year)
+exam_st_date = (11, 12, 2024)
+
+# Print the exam start date using string formatting
+# The '%i' placeholders are filled with the values from the 'exam_st_date' tuple
+print("The examination will start from : %i / %i / %i" % exam_st_date)
