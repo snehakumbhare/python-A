9162202f114c02e9934e5ade5655de5c5864dff7
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-04-16 14:48:54 +0530

    Update Mod.py

--- Mod.py
+++ Mod.py
@@ -1,6 +1,7 @@
 #Python: Whether an integer greater than 4^4 which is 4 mod 34
 
 #Write a Python program that accepts an integer and determines whether it is greater than 4^4 and which is 4 mod 34.
+
 # License: https://bit.ly/3oLErEI
 
 # Define a function named 'test' that takes an integer 'n' as input
