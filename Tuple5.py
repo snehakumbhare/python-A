16fca20cc88cb041fa75de7f2f7db6933edde2cc
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-04-30 15:55:51 +0530

    Update Tuple5.py

--- Tuple5.py
+++ Tuple5.py
@@ -1,6 +1,7 @@
 #Python Exercise: Create a tuple
 
 #Write a  Python program to create a tuple.
+
 # Create an empty tuple and assign it to the variable 'x'
 x = ()
 # Print the contents of the 'x' tuple, which is empty
