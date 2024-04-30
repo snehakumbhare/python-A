5d7e54b1bd6924f50a7b66707e86c6a6a68c5770
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-04-30 15:54:57 +0530

    Add files via upload

---
+++ tuple5.py
@@ -0,0 +1,11 @@
+#Python Exercise: Create a tuple
+#Write a  Python program to create a tuple.
+# Create an empty tuple and assign it to the variable 'x'
+x = ()
+# Print the contents of the 'x' tuple, which is empty
+print(x)
+
+# Create an empty tuple using the tuple() constructor and assign it to the variable 'tuplex'
+tuplex = tuple()
+# Print the contents of the 'tuplex' tuple, which is also empty
+print(tuplex) 
