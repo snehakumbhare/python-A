7c348f289a36f8eee83af43ef5fe7083e8a02694
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-05-01 16:01:45 +0530

    Update Tuple6.py

--- Tuple6.py
+++ Tuple6.py
@@ -1,6 +1,7 @@
 #Python Exercise: Create a tuple with numbers, and display a member
 
 #Write a  Python program to create a tuple of numbers and print one item.
+
 # Create a tuple containing a sequence of numbers
 tuplex = 5, 10, 15, 20, 25
 # Print the contents of the 'tuplex' tuple
