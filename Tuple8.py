23dbaf5986253540abdf6bbf39cc6c61a41d06bf
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-05-03 16:26:21 +0530

    Update Tuple8.py

--- Tuple8.py
+++ Tuple8.py
@@ -1,6 +1,7 @@
 #Python Exercise: Tuple to a string
 
 #Write a  Python program to convert a tuple to a string.
+
 # Create a tuple containing individual characters
 tup = ('e', 'x', 'e', 'r', 'c', 'i', 's', 'e', 's')
 # Use the 'join' method to concatenate the characters in the tuple without any spaces and create a single string
