6898e7c4218ee877f63020e16919ce05aa812bcc
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-05-05 16:11:34 +0530

    Add files via upload

---
+++ tuple10.py
@@ -0,0 +1,11 @@
+#Python Exercise: Convert a list to a tuple
+#Write a  Python program to convert a list to a tuple.
+# Create a list containing a sequence of numbers
+listx = [5, 10, 7, 4, 15, 3]
+# Print the contents of the 'listx' list
+print(listx)
+
+# Use the 'tuple()' function, a built-in Python function, to convert the 'listx' list to a tuple
+tuplex = tuple(listx)
+# Print the contents of the 'tuplex' tuple
+print(tuplex)
