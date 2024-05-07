0403a52330022ff7f6d786713a1cc592961dbc89
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-05-07 15:30:57 +0530

    Add files via upload

---
+++ tuple12.py
@@ -0,0 +1,11 @@
+#Python Exercise: Convert a tuple to a dictionary
+#Write a  Python program to convert a tuple to a dictionary.
+# Create a tuple containing nested tuples, where each inner tuple consists of two elements.
+tuplex = ((2, "w"), (3, "r"))
+
+# Create a dictionary by using a generator expression to swap the elements of each inner tuple.
+# The generator iterates through 'tuplex', and for each inner tuple (x, y), it creates a key-value pair (y, x).
+result_dict = dict((y, x) for x, y in tuplex)
+
+# Print the resulting dictionary.
+print(result_dict) 
