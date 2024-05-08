beefdcbf1264538f3c9a5fee96099839af858431
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-05-08 17:04:59 +0530

    Add files via upload

---
+++ tuple13.py
@@ -0,0 +1,12 @@
+#Python Exercise: Unzip a list of tuples into individual lists
+#Write a  Python program to unzip a list of tuples into individual lists.
+# Create a list of tuples, where each tuple contains two elements.
+l = [(1, 2), (3, 4), (8, 9)]
+
+# Use the 'zip' function with the '*' operator to unpack and zip the tuples.
+# This creates new tuples where the first elements from the original tuples are combined into one tuple,
+# and the second elements from the original tuples are combined into another tuple.
+result = list(zip(*l))
+
+# Print the result, which is a list of two tuples formed by zipping the original tuples.
+print(result)
