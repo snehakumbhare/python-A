49c64204145b92acb6356f034b952a43076df7ae
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-05-11 16:01:08 +0530

    Add files via upload

---
+++ tuple15.py
@@ -0,0 +1,7 @@
+#Python Exercise: Print a tuple with string formatting
+#Write a Python program to print a tuple with string formatting.
+# Create a tuple containing three numbers
+t = (100, 200, 300)
+
+# Use the 'format' method to insert the tuple 't' into the string and print the result
+print('This is a tuple {0}'.format(t))
