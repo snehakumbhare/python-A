2f8b63127c1307c9ea5b3d96060a331dd8ef963c
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-05-13 16:13:26 +0530

    Update Tuple16.py

--- Tuple16.py
+++ Tuple16.py
@@ -1,6 +1,7 @@
 #Python Exercise: Remove an empty tuple(s) from a list of tuples
 
 #Write a Python program to remove an empty tuple(s) from a list of tuples.
+
 # Create a list 'L' containing various elements, including empty tuples and tuples with strings.
 
 # Use a list comprehension to filter out the empty tuples by checking if each tuple 't' in 'L' is not empty.
