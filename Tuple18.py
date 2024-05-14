9193421d62d555155e78c5eaa6ef33046fb2b163
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-05-14 15:46:38 +0530

    Update Tuple18.py

--- Tuple18.py
+++ Tuple18.py
@@ -1,4 +1,5 @@
 #Python Exercise: Sort a tuple by its float element
+
 #Write a  Python program to sort a tuple by its float element.
 # Create a list of tuples 'price', where each tuple represents an item and its price as a string.
 price = [('item1', '12.20'), ('item2', '15.10'), ('item3', '24.5')]
