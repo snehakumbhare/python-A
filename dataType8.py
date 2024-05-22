6080c9ece9e306338fd49194064368cb90b7c6bb
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-05-22 15:32:31 +0530

    Add files via upload

---
+++ dataType8.py
@@ -0,0 +1,9 @@
+#Python: Check a list is empty or not
+#Write a  Python program to check if a list is empty or not.
+# Create an empty list 'l'
+l = []
+
+# Check if the list 'l' is empty using the 'not' keyword
+if not l:
+    # If the list is empty, print the message "List is empty"
+    print("List is empty")
