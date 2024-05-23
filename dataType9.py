d3d23b7d5c0cb812458238280e4cd96d2ea14840
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-05-23 16:04:00 +0530

    Add files via upload

---
+++ dataType9.py
@@ -0,0 +1,13 @@
+#Python: Clone or copy a list
+#Write a Python program to clone or copy a list.
+# Create a list 'original_list' with some elements
+original_list = [10, 22, 44, 23, 4]
+
+# Create a new list 'new_list' and copy the elements from 'original_list' into it
+new_list = list(original_list)
+
+# Print the contents of the 'original_list'
+print(original_list)
+
+# Print the contents of the 'new_list', which is a copy of 'original_list'
+print(new_list)
