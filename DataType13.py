c4430a920fb7812fc8ac7b23cc35f5d601834496
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-05-28 16:07:38 +0530

    Update DataType13.py

--- DataType13.py
+++ DataType13.py
@@ -1,4 +1,5 @@
 #Python: Generate a 3D array
+
 #Write a  Python program to generate a 3*4*6 3D array whose each element is *.
 # Create a 3D array using list comprehensions, with 3 rows, 4 columns, and each element initialized as '*'
 array = [[['*' for col in range(6)] for col in range(4)] for row in range(3)]
