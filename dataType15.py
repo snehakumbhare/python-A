f1034d5b6378439eb4b7b2a291e05cd97bc2e309
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-05-30 15:37:00 +0530

    Add files via upload

---
+++ dataType15.py
@@ -0,0 +1,13 @@
+#Python: Shuffle and print a specified list
+#Write a  Python program to shuffle and print a specified list.
+# Import the 'shuffle' function from the 'random' module, which is used for shuffling the elements in a list
+from random import shuffle
+
+# Create a list 'color' with several color strings
+color = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+
+# Use the 'shuffle' function to randomly reorder the elements in the 'color' list
+shuffle(color)
+
+# Print the shuffled 'color' list, which will have its elements in a random order
+print(color)
