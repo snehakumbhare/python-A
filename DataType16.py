e1cf22cffb78c594435bf71e09c14ada5726813d
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-05-31 15:42:49 +0530

    Update DataType16.py

--- DataType16.py
+++ DataType16.py
@@ -1,4 +1,5 @@
 #Python: Generate and print a list of first and last 5 elements where the values are square of numbers between two numbers
+
 #Write a  Python program to generate and print a list of the first and last 5 elements where the values are square numbers between 1 and 30 (both included).
 # Define a function named printValues
 def printValues():
