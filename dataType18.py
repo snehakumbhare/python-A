6f41c8589c31c2ad8e3598429f3a568c90254362
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-06-02 16:06:30 +0530

    Add files via upload

---
+++ dataType18.py
@@ -0,0 +1,8 @@
+#Python: Generate all permutations of a list in Python
+#Write a  Python program to generate all permutations of a list in  Python.
+# Import the 'itertools' module, which provides various functions for working with iterators
+import itertools
+
+# Use 'itertools.permutations' to generate all permutations of the list [1, 2, 3], and convert the result to a list
+# This will produce all possible orderings of the elements in the list
+print(list(itertools.permutations([1, 2, 3])))
