4340e24de8297115e220d97720c3443031609bc1
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-06-02 16:07:30 +0530

    Update DataType18.py

--- DataType18.py
+++ DataType18.py
@@ -1,4 +1,5 @@
 #Python: Generate all permutations of a list in Python
+
 #Write a  Python program to generate all permutations of a list in  Python.
 # Import the 'itertools' module, which provides various functions for working with iterators
 import itertools
