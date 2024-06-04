5253da09fbee485fbb0de2e5829c44d7b6235a14
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-06-04 15:43:31 +0530

    Update DataType20.py

--- DataType20.py
+++ DataType20.py
@@ -1,6 +1,7 @@
 #Python: Access the index of a list
 
 #Write a  Python program to access the index of a list.
+
 # Define a list 'nums' containing a sequence of numbers
 nums = [5, 15, 35, 8, 98]
 # Use a 'for' loop with 'enumerate' to iterate through 'nums' and obtain both the index and value of each element
