1695df1a7f833dca0ca342754c93dea3878f1591
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-06-05 16:31:40 +0530

    Add files via upload

---
+++ dataType21.py
@@ -0,0 +1,11 @@
+#Python: Convert a list of characters into a string
+#Write a  Python program to convert a list of characters into a string.
+# Define a list 's' containing individual characters
+s = ['a', 'b', 'c', 'd']
+
+# Use the 'join' method to concatenate the characters in the list 's' into a single string
+str1 = ''.join(s)
+
+# Print the concatenated string 'str1'
+print(str1)
+
