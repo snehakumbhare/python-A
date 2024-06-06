68c3d60db6e1c0c7b64e64e811d9fda58bdaab8c
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-06-06 16:19:08 +0530

    Update DataType22.py

--- DataType22.py
+++ DataType22.py
@@ -1,6 +1,7 @@
 #Python: Find the index of an item in a specified list
 
 #Write a  Python program to find the index of an item in a specified list.
+
 # Define a list 'num' containing a sequence of numbers
 num = [10, 30, 4, -6]
 
