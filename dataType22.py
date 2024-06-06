74ebb57fef1cee02561ea234888a4769072a8fe2
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-06-06 16:18:26 +0530

    Add files via upload

---
+++ dataType22.py
@@ -0,0 +1,8 @@
+#Python: Find the index of an item in a specified list
+#Write a  Python program to find the index of an item in a specified list.
+# Define a list 'num' containing a sequence of numbers
+num = [10, 30, 4, -6]
+
+# Use the 'index' method to find the index of the element with the value 30 in the list 'num'
+# The result will be the position of the first occurrence of 30 in the list
+print(num.index(30))
