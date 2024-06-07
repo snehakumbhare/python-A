22d99f8d12e790439eafbcc425080d847d486c1a
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-06-07 17:02:14 +0530

    Add files via upload

---
+++ dataType23.py
@@ -0,0 +1,13 @@
+#Python: Flatten a shallow list
+#Write a  Python program to flatten a shallow list.
+# Import the 'itertools' module, which provides various functions for working with iterators
+import itertools
+
+# Define a list 'original_list' containing nested sublists
+original_list = [[2, 4, 3], [1, 5, 6], [9], [7, 9, 0]]
+
+# Use 'itertools.chain' and the unpacking operator (*) to merge the sublists into a single flat list
+new_merged_list = list(itertools.chain(*original_list))
+
+# Print the newly merged list 'new_merged_list'
+print(new_merged_list)
