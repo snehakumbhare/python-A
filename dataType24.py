b51e43674e17e517cc1f79bb03cd15366ec336f5
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-06-08 17:02:09 +0530

    Add files via upload

---
+++ dataType24.py
@@ -0,0 +1,13 @@
+#Python: Append a list to the second list
+#Write a  Python program to append a list to the second list.
+# Define a list 'list1' containing numeric elements
+list1 = [1, 2, 3, 0]
+
+# Define another list 'list2' containing string elements
+list2 = ['Red', 'Green', 'Black']
+
+# Concatenate 'list1' and 'list2' to create a single list 'final_list'
+final_list = list1 + list2
+
+# Print the 'final_list,' which contains elements from both 'list1' and 'list2'
+print(final_list)
