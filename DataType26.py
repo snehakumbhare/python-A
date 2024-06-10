06d9d579c31072a545941aa65fc6a66b15a696ef
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-06-10 15:23:36 +0530

    Update DataType26.py

--- DataType26.py
+++ DataType26.py
@@ -1,4 +1,5 @@
 #Python: Check whether two lists are circularly identical
+
 #Write a Python program to check whether two lists are circularly identical.
 # Define three lists: list1, list2, and list3, each containing a sequence of numbers
 list1 = [10, 10, 0, 0, 10]
