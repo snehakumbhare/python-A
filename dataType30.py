63279e6cf96604314b61c2297856a6cc652ba737
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-06-14 15:54:42 +0530

    Add files via upload

---
+++ dataType30.py
@@ -0,0 +1,17 @@
+#Python: Get the frequency of the elements in a list
+#Write a  Python program to get the frequency of elements in a list.
+# Import the 'collections' module, which provides specialized container data types
+import collections
+
+# Define a list 'my_list' containing multiple numbers, including duplicates
+my_list = [10, 10, 10, 10, 20, 20, 20, 20, 40, 40, 50, 50, 30]
+
+# Print the original list 'my_list'
+print("Original List : ", my_list)
+
+# Use the 'collections.Counter' function to count the frequency of each element in 'my_list' and store it in 'ctr'
+ctr = collections.Counter(my_list)
+
+# Print the frequency of the elements in the list, as provided by the 'ctr' object
+print("Frequency of the elements in the List : ", ctr) 
+
