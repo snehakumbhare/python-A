31af5c59ff57458c5d4591097e3f0fe60af9efea
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-06-26 16:05:15 +0530

    Add files via upload

---
+++ dataType39.py
@@ -0,0 +1,13 @@
+#Python: Convert a list of multiple integers into a single integer
+#Write a  Python program to convert a list of multiple integers into a single integer.
+# Define a list 'L' containing numeric elements
+L = [11, 33, 50]
+
+# Print the original list 'L'
+print("Original List: ", L)
+
+# Use the 'join' function to convert the elements of 'L' to a single string, then convert it to an integer 'x'
+x = int("".join(map(str, L)))
+
+# Print the single integer 'x'
+print("Single Integer: ", x)
