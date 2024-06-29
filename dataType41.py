621bfbe8f792ee32cfb0cba17f64de0ff6d5e631
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-06-29 16:30:50 +0530

    Add files via upload

---
+++ dataType41.py
@@ -0,0 +1,12 @@
+#Python: Create multiple lists
+#Write a  Python program to create multiple lists.
+#Create an empty dictionary named 'obj'
+obj = {}
+
+# Iterate through the range of numbers from 1 to 20
+for i in range(1, 21):
+    # Create an empty list as the value and associate it with the string representation of the number 'i' as the key in the dictionary 'obj'
+    obj[str(i)] = []
+
+# Print the resulting dictionary 'obj'
+print(obj) 
