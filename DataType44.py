edbeead2d28a098e1e085c67ac30899b3f299606
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-07-04 16:43:31 +0530

    Update DataType44.py

--- DataType44.py
+++ DataType44.py
@@ -1,4 +1,5 @@
 #Python: Generate groups of five consecutive numbers in a list
+
 #Write a  Python program to generate groups of five consecutive numbers in a list.
 # Create a nested list 'l' using a list comprehension
 # This list comprehension generates a 5x5 matrix with elements calculated using the formula 5*i + j
