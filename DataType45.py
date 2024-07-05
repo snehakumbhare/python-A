2f7798d7bd8faa8e82c6801c85d9449fe7a2c03a
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-07-05 15:43:24 +0530

    Update DataType45.py

--- DataType45.py
+++ DataType45.py
@@ -1,6 +1,7 @@
 #Python: Convert a pair of values into a sorted unique array
 
 #Write a  Python program to convert a pair of values into a sorted unique array.
+
 # Define a list 'L' containing tuples with pairs of numbers
 L = [(1, 2), (3, 4), (1, 2), (5, 6), (7, 8), (1, 2), (3, 4), (3, 4), (7, 8), (9, 10)]
 
