a4b75ce7cad47f35506f4f3e48197729d2b9e815
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-07-06 16:01:32 +0530

    Add files via upload

---
+++ dataType46.py
@@ -0,0 +1,10 @@
+#Python: Select the odd items of a list
+#Write a  Python program to select the odd items from a list.
+# Define a list 'x' containing numeric elements from 1 to 9
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# Use slicing to create a new list containing every second element of 'x'
+# The slice notation 'x[::2]' starts at the beginning, ends at the end, and steps by 2
+# It selects every second element in the list
+# Print the resulting list
+print(x[::2])
