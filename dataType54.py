9cf092e9655c8a5eadfabf46c010f33be74701f5
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-07-18 16:13:54 +0530

    Add files via upload

---
+++ dataType54.py
@@ -0,0 +1,12 @@
+#Python: Concatenate elements of a list
+#Write a Python program to concatenate elements of a list.
+# Define a list 'color' containing color names as strings
+color = ['red', 'green', 'orange']
+
+# Use the 'join' method to concatenate the elements of the 'color' list with a hyphen '-' in between
+# Print the resulting string
+print('-'.join(color))
+
+# Use the 'join' method to concatenate the elements of the 'color' list without any separator
+# Print the resulting string
+print(''.join(color)) 
