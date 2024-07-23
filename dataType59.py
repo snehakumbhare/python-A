7754cb43df368f4b0513d6f16cef8859a947f576
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-07-23 15:26:06 +0530

    Add files via upload

---
+++ dataType59.py
@@ -0,0 +1,10 @@
+#Python: Check whether the n-th element exists in a given list
+#Write a Python program to check whether the n-th element exists in a given list.
+# Define a list 'x' containing integers
+x = [1, 2, 3, 4, 5, 6]
+
+# Calculate the length of the list 'x' using the 'len' function and subtract 1 to get the last element's index
+xlen = len(x) - 1
+
+# Print the last element of the list 'x' using its index, which is 'xlen'
+print(x[xlen])
