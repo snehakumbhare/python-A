b76501bea681a44695b71dee58a2242680cf816b
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-07-29 16:04:05 +0530

    Add files via upload

---
+++ dataType62.py
@@ -0,0 +1,8 @@
+#Python: Print a list of space-separated elements
+#Write a  Python program to print a list of space-separated elements.
+# Define a list 'num' containing integers
+num = [1, 2, 3, 4, 5]
+
+# Use the '*' operator in a print statement to unpack the elements of the list 'num' as separate arguments
+# This will print each element of the list separated by spaces
+print(*num)
