280bc0cfd8e5f166b3e04a352f54284c03cb1067
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-07-30 16:00:59 +0530

    Add files via upload

---
+++ dataType63.py
@@ -0,0 +1,11 @@
+#Python: Insert a given string at the beginning of all items in a list
+#Write a Python program to insert a given string at the beginning of all items in a list.
+# Define a list 'num' containing integers
+num = [1, 2, 3, 4]
+
+# Use a list comprehension to create a new list of strings, where each string is formed by appending the index (formatted as a string) to the string 'emp'
+# This effectively generates a list of strings with elements like 'emp1', 'emp2', 'emp3', 'emp4'
+new_list = ['emp{0}'.format(i) for i in num]
+
+# Print the new list 'new_list' containing the formatted strings
+print(new_list)
