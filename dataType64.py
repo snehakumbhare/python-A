0df99e1a18b98b8699dd388c442bd649f02275b2
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-07-31 15:56:20 +0530

    Add files via upload

---
+++ dataType64.py
@@ -0,0 +1,12 @@
+#Python: Iterate over two lists simultaneously
+#Write a Python program to iterate over two lists simultaneously.
+# Define two lists 'num' and 'color' containing integers and color names, respectively
+num = [1, 2, 3]
+color = ['red', 'white', 'black']
+
+# Use a 'for' loop and the 'zip' function to iterate over pairs of elements from 'num' and 'color' in parallel
+# In each iteration, the variables 'a' and 'b' will hold the values from 'num' and 'color', respectively
+# Print the values of 'a' and 'b' in each iteration
+for (a, b) in zip(num, color):
+    print(a, b)
+
