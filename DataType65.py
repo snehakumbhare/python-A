c9682f418211b16355c97a9976995dc9f204bf57
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-08-01 14:47:56 +0530

    Update DataType65.py

--- DataType65.py
+++ DataType65.py
@@ -1,4 +1,5 @@
 #Python: Move all zero digits to end of a given list of numbers
+
 #Write a  Python program to move all zero digits to the end of a given list of numbers.
 # Define a function 'test' that takes a list 'lst' as an argument
 def test(lst):
