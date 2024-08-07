ded3cf46b3b4d3035f0582c02d3d28a306a7e6cb
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-08-07 15:49:21 +0530

    Add files via upload

---
+++ dataType71.py
@@ -0,0 +1,15 @@
+#Python: Check whether all dictionaries in a list are empty or not
+#Write a  Python program to check whether all dictionaries in a list are empty or not
+# Define a list 'my_list' containing three empty dictionaries
+my_list = [{}, {}, {}]
+
+# Define another list 'my_list1' containing dictionaries, one of which has a key-value pair
+my_list1 = [{1: 2}, {}, {}]
+
+# Check if all dictionaries in 'my_list' are empty (i.e., contain no key-value pairs)
+# The 'not d for d in my_list' generator expression returns 'True' for each empty dictionary, and 'all' checks if they are all 'True'
+print(all(not d for d in my_list))
+
+# Check if all dictionaries in 'my_list1' are empty (i.e., contain no key-value pairs)
+# The 'not d for d in my_list1' generator expression returns 'True' for the dictionaries that are empty, and 'all' checks if they are all 'True'
+print(all(not d for d in my_list1)) 
