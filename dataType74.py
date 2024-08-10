17e6c23352a7756ffa27c7bbb81e66bff6a301c9
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-08-10 17:02:56 +0530

    Update dataType74.py

--- dataType74.py
+++ dataType74.py
@@ -1,4 +1,5 @@
 #Python: Pack consecutive duplicates of a given list elements into sublists
+
 #Write a  Python program to pack consecutive duplicates of a given list of elements into sublists.
 # Import the 'groupby' function from the 'itertools' module
 from itertools import groupby
