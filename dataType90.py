8a79855aeb36619afd1a5eab1193647d000f259a
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-08-29 15:27:43 +0530

    Update dataType90.py

--- dataType90.py
+++ dataType90.py
@@ -1,4 +1,5 @@
 #Python: Count number of lists in a given list of lists
+
 #Write a  Python program to count the number of lists in a given list of lists.
 # Define a function 'count_list' that calculates the number of lists in the input list
 def count_list(input_list):
