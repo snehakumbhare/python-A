a45d59e6e1d2924e485bcea5b2ddea7b28bb8ea6
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-08-30 16:12:49 +0530

    Update DataType91.py

--- DataType91.py
+++ DataType91.py
@@ -1,6 +1,7 @@
 #Python: Find the list with maximum and minimum length
 
 #Write a  Python program to find a list with maximum and minimum lengths.
+
 # Define a function 'max_length_list' that finds the maximum length and the list with the maximum length in 'input_list'
 def max_length_list(input_list):
     # Find the maximum length of lists in 'input_list'
