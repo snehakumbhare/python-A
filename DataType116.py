8e1771f1a8ed3b325e4b19c94a449e9588b1f8cb
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-09-26 15:48:57 +0530

    Update DataType116.py

--- DataType116.py
+++ DataType116.py
@@ -1,4 +1,5 @@
 #Python: Sort a list of lists by a given index of the inner list
+
 #Write a  Python program to sort a list of lists by a given index of the inner list.
 # Import the 'itemgetter' function from the 'operator' module
 from operator import itemgetter
