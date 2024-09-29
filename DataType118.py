266475306cf66617bfdc1e6b0e256aa0aff55bb1
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-09-29 16:20:18 +0530

    Update DataType118.py

--- DataType118.py
+++ DataType118.py
@@ -1,4 +1,5 @@
 #Python: Find the difference between elements (n+1th – nth) of a given list of numeric values
+
 #Write a  Python program to find the difference between elements (n+1th – nth) of a given list of numeric values.
 # Define a function 'elements_difference' that calculates the differences between adjacent elements in a list
 def elements_difference(nums):
