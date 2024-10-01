af1c8b80d6e74040856f14a959e1a555d174f88a
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-10-01 15:37:06 +0530

    Update DataType120.py

--- DataType120.py
+++ DataType120.py
@@ -1,6 +1,7 @@
 #Python: Create a list taking alternate elements from a given list
 
 #Write a  Python program to create a list taking alternate elements from a given list.
+
 # Define a function 'alternate_elements' that returns a list of alternate elements from the input list
 def alternate_elements(list_data):
     # Initialize an empty list 'result' to store the alternate elements
