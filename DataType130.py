c1c8219aef88b0bba4d5cbce8340fd6c670da1d2
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-10-14 19:02:29 +0530

    Update DataType130.py

--- DataType130.py
+++ DataType130.py
@@ -1,6 +1,7 @@
 #Python: Count the same pair in three given lists
 
 #Write a Python program to count the same pair in three given lists.
+
 # Define a function 'count_same_pair' that counts the number of same pairs between three lists
 def count_same_pair(nums1, nums2, nums3):
     # Use a generator expression within the 'sum' function to count the same pairs
