384e50799585639fb2cfc32eeb8b402d1abd37a0
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-10-23 16:53:02 +0530

    Update DataType138.py

--- DataType138.py
+++ DataType138.py
@@ -1,4 +1,5 @@
 #Python: Sort a given mixed list of integers and strings
+
 #Write a  Python program to sort a given mixed list of integers and strings. Numbers must be sorted before strings.
 # Define a function 'sort_mixed_list' that sorts a mixed list of integers and strings
 def sort_mixed_list(mixed_list):
