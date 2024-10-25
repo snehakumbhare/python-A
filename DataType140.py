568b22afdfd9d1f4f86ad59d0d4cd64bf1c4f1d9
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-10-25 16:58:43 +0530

    Update DataType140.py

--- DataType140.py
+++ DataType140.py
@@ -1,4 +1,5 @@
 #Python: Remove the specific item from a given list of lists
+
 #Write a Python program to remove a specific item from a given list of lists.
 # Import the 'copy' module
 import copy	
