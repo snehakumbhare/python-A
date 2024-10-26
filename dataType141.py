450f9006126c1d47e58a1b3e6554d88be46d00aa
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-10-26 15:08:05 +0530

    Add files via upload

---
+++ dataType141.py
@@ -0,0 +1,18 @@
+#Python: Remove empty lists from a given list of lists
+#Write a  Python program to remove empty lists from a given list of lists.
+# Create a list 'list1' that contains a mix of empty lists, strings, and sublists
+list1 = [[], [], [], 'Red', 'Green', [1, 2], 'Blue', [], []]
+
+# Print a message indicating the original list
+print("Original list:")
+# Print the contents of 'list1'
+print(list1)
+
+# Print a message indicating the operation to remove empty lists
+print("\nAfter deleting the empty lists from the said lists of lists")
+
+# Create a new list 'list2' containing non-empty elements from 'list1'
+list2 = [x for x in list1 if x]
+
+# Print 'list2', which contains non-empty elements
+print(list2)
