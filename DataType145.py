7eb6cd597f6b83ecb8a7f93469c40b77912abd73
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-10-30 16:43:38 +0530

    Update DataType145.py

--- DataType145.py
+++ DataType145.py
@@ -1,4 +1,5 @@
 #Python: Generate a number in a specified range except some specific numbers
+
 #Write a  Python program to generate a number in a specified range except for some specific numbers.
 ## Import the 'choice' function from the 'random' module
 from random import choice
