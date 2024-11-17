624da5258d26af0b8d31433913a4be8cff632e33
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-11-17 16:39:55 +0530

    Update DataType161.py

--- DataType161.py
+++ DataType161.py
@@ -3,7 +3,6 @@
 #Write a  Python program to check if a given list increases strictly. Moreover, if removing only one element 
 #from the list results in a strictly increasing list, we still consider the list true.
 
-# Source: https://bit.ly/3qZqcwm
 # Define a function called 'almost_increasing_sequence' that checks if a given sequence is an almost increasing sequence.
 def almost_increasing_sequence(sequence):
     # Check if the length of the sequence is less than 3, and if so, return True.
