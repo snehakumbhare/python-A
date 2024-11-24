f1718f40ccdf70375944f130df63ee3029ea4991
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-11-24 16:14:34 +0530

    Update DataType168.py

--- DataType168.py
+++ DataType168.py
@@ -1,4 +1,5 @@
 #Python: Display each element vertically of a given list, list of lists
+
 #Write a  Python program to display vertically each element of a given list, list of lists.
 # Create a list 'text' containing string elements.
 text = ["a", "b", "c", "d", "e", "f"]
