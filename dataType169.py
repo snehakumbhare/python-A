d8e3983e6c6bae48037740b020b99f46b7c271c0
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-11-25 15:24:35 +0530

    Add files via upload

---
+++ dataType169.py
@@ -0,0 +1,20 @@
+#Python: Convert a given list of strings and characters to a single list of characters
+#Write a  Python program to convert a given list of strings and characters to a single list of characters.
+# Define a function called 'l_strs_to_l_chars' that converts a list of strings and characters into a single list of characters.
+def l_strs_to_l_chars(lst):
+    # Use a nested list comprehension to create a new list 'result' where each character of the input elements is a separate element in the list.
+    result = [i for element in lst for i in element]
+    return result
+
+# Create a list 'colors' containing string and character elements.
+colors = ["red", "white", "a", "b", "black", "f"]
+
+# Print a message indicating the original list.
+print("Original list:")
+print(colors)
+
+# Print a message indicating the intention to convert the list of strings and characters into a single list of characters.
+print("\nConvert the said list of strings and characters to a single list of characters:")
+
+# Call the 'l_strs_to_l_chars' function to perform the conversion and print the result.
+print(l_strs_to_l_chars(colors)) 
