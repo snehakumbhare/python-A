6330264122b80059a9f60f4e5e51f166c43aad91
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-11-26 16:00:48 +0530

    Update dataType170.py

--- dataType170.py
+++ dataType170.py
@@ -1,6 +1,7 @@
 #Python: Insert an element in a given list after every nth position
 
 #Write a  Python program to insert an element in a given list after every nth position.
+
 # Define a function called 'insert_elemnt_nth' that inserts an element 'ele' into a list 'lst' after every 'n' elements.
 def insert_elemnt_nth(lst, ele, n):
     # Create an empty list 'result' to store the modified list.
