62741a496d55bee1b17b633977f68d2ef21df2c7
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-11-28 15:55:45 +0530

    Update dataType172.py

--- dataType172.py
+++ dataType172.py
@@ -1,4 +1,5 @@
 #Python: Remove the last N number of elements from a given list
+
 #Write a  Python program to remove the last N number of elements from a given list.
 # Define a function called 'remove_last_n' that removes the last 'N' elements from a list 'nums'.
 def remove_last_n(nums, N):
