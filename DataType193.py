a6c179d2f5908a9b4bf44c4df1d7ac036cecdaf0
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-12-21 16:31:45 +0530

    Update DataType193.py

--- DataType193.py
+++ DataType193.py
@@ -1,4 +1,5 @@
 #Python: Find the dimension of a given matrix
+
 #Write a  Python program to find the dimension of a given matrix.
  # Define a function called 'matrix_dimensions' that calculates the number of rows and columns in a matrix (list of lists).
 def matrix_dimensions(test_list):
