7923fb70aa26afb08cb02c41d87bacf95c18b224
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2024-12-31 15:40:01 +0530

    Update DataType203.py

--- DataType203.py
+++ DataType203.py
@@ -1,6 +1,7 @@
 #Python: Check if first digit/character in each element in a given list is same or not
 
 #Write a Python program to check if the first digit or character of each element in a list is the same.
+
 # Define a function 'test' that checks if the first character (or digit) in each element of the given list is the same.
 def test(lst):
     # Use a generator expression with 'all' to check if the first character (or digit) of each element matches the first element.
